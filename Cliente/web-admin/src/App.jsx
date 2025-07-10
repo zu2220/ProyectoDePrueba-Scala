c729aaeca069e83a451cfcc92ba7b227ca5bb453
Oscar Zari <U22206853@utp.edu.pe>
2025-07-09 23:22:58 -0500

    cambios: se envolvió a las rutas con el UserProvider

--- Cliente/web-admin/src/App.jsx
+++ Cliente/web-admin/src/App.jsx
@@ -2,29 +2,32 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { OrderProvider } from './context/OrderContext'; 
+import { OrderProvider } from './context/OrderContext';
 import DashboardPage from './pages/DashboardPage';
 import UsersPage from './pages/UsersPage';
 import OrdersPage from './pages/OrdersPage';
 import ProductsPage from './pages/ProductsPage';
-import CustomersPage from './pages/CustomersPage'; 
-import SuppliersPage from './pages/SuppliersPage'; 
+import CustomersPage from './pages/CustomersPage';
+import SuppliersPage from './pages/SuppliersPage';
 import SuppliesPage from './pages/SuppliesPage';
+import { UserProvider } from './context/UserContext';
 
 function App() {
   return (
     <Router>
       <OrderProvider>
+        <UserProvider>
         <Routes>
-          <Route path="/" element={<DashboardPage />} /> 
+          <Route path="/" element={<DashboardPage />} />
           <Route path="/dashboard" element={<DashboardPage />} />
           <Route path="/orders" element={<OrdersPage />} />
-          <Route path="/users" element={<UsersPage />} /> 
+            <Route path="/users" element={<UsersPage />} />
           <Route path="/products" element={<ProductsPage />} />
-          <Route path="/clients" element={<CustomersPage />} /> 
+          <Route path="/clients" element={<CustomersPage />} />
           <Route path="/providers" element={<SuppliersPage />} />
           <Route path="/supplies" element={<SuppliesPage />} />
         </Routes>
+        </UserProvider>
       </OrderProvider>
     </Router>
   );
